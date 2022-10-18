a95eb25f88a1cfbccdea69fec75d5e7fefe802f1
Vitaly Zorin <VIZorin@stud.kpfu.ru>
2022-10-18 21:42:55 +0300

    + base page

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route('/')
+def main():
+    return render_template('base.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
