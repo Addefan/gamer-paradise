fc52461b4860f12a0df608b082d06ddb12f41bff
Vitaly Zorin <VIZorin@stud.kpfu.ru>
2022-11-03 12:56:53 +0300

    + app initialization structuring

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
 
 import database as db
 from auth import auth
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-bootstrap = Bootstrap5(app)
-app.register_blueprint(auth)
-db.init_app(app)
+from extensions import bootstrap, lm
 
 
-@app.route('/')
-def main():
-    return render_template('base.html')
+def create_app(config=Config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    register_extensions(app)
+    register_blueprints(app)
+
+    @app.route('/')
+    def main():
+        return render_template('base.html')
+
+    return app
+
+
+def register_extensions(app):
+    bootstrap.init_app(app)
+    lm.init_app(app)
+    db.init_app(app)
+
+
+def register_blueprints(app):
+    app.register_blueprint(auth)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    create_app().run(debug=True)
---
+++ extensions.py
@@ -0,0 +1,5 @@
+from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager
+
+bootstrap = Bootstrap5()
+lm = LoginManager()
