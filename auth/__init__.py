db7638ec84561c077a26a516be958d2d7d86c918
Vitaly Zorin <VIZorin@stud.kpfu.ru>
2022-11-04 03:42:36 +0300

    + register, login, logout views

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,3 +1,15 @@
 from flask import Blueprint
 
+from extensions import lm
+
 auth = Blueprint('auth', __name__, template_folder='templates')
+
+from auth import views
+
+lm.login_view = 'auth.login'
+lm.login_message = 'Чтобы просмотреть эту страницу, необходимо авторизоваться'
+lm.login_message_category = 'error'
+
+auth.add_url_rule('/register', view_func=views.RegisterView.as_view('register'))
+auth.add_url_rule('/login', view_func=views.LoginView.as_view('login'))
+auth.add_url_rule('/logout', view_func=views.LogoutView.as_view('logout'))
---
+++ auth/views.py
@@ -0,0 +1,81 @@
+from flask import render_template, redirect, url_for, flash, request
+from flask.views import MethodView
+from flask_login import login_user, logout_user, current_user, login_required
+from psycopg2 import errors
+from psycopg2.errorcodes import UNIQUE_VIOLATION
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from auth.forms import RegisterForm, LoginForm
+from auth.user_login import UserLogin
+from database import get_db
+from extensions import lm
+
+
+@lm.user_loader
+def load_user(user_id):
+    return UserLogin().get_user(get_db(), user_id=user_id)
+
+
+class RegisterView(MethodView):
+    def get(self):
+        if current_user.is_authenticated:
+            flash('Вы уже зарегистрированы', 'warning')
+            return redirect(url_for('main'))  # TODO изменить ссылку на профиль
+
+        form = RegisterForm()
+        return render_template('auth/auth.html', action='Регистрация', form=form)
+
+    def post(self):
+        form = RegisterForm()
+        if form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            db = get_db()
+            try:
+                db.insert('INSERT INTO users (email, password) VALUES (%s, %s);',
+                          (email, generate_password_hash(password)))
+            except errors.lookup(UNIQUE_VIOLATION):
+                form.email.errors.append('Пользователь с такой почтой уже существует')
+            else:
+                user = UserLogin().get_user(db, email=email)
+                login_user(user)
+                flash('Вы успешно зарегистрированы', 'success')
+                return redirect(url_for('login'))
+        return render_template('auth/auth.html', action='Регистрация', form=form)
+
+
+class LoginView(MethodView):
+    def get(self):
+        if current_user.is_authenticated:
+            flash('Вы уже авторизованы', 'warning')
+            return redirect(url_for('main'))  # TODO изменить ссылку на профиль
+
+        form = LoginForm()
+        return render_template('auth/auth.html', action='Авторизация', form=form)
+
+    def post(self):
+        form = LoginForm()
+        if form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            remember = form.remember.data
+            db = get_db()
+            user = db.select(f"SELECT * FROM users WHERE email = '{email}'")  # TODO: пофиксить селект: возможны инъекции
+            if user and check_password_hash(user['password'], password):
+                user = UserLogin().get_user(db, email=email)
+                login_user(user, remember)
+                flash('Вы успешно авторизованы', 'success')
+                return redirect(request.args.get('next') or url_for('main'))  # TODO: исправить редирект на товары
+            else:
+                form.email.errors.append('Неверный адрес электронной почты или пароль')
+                form.password.errors.append('Неверный адрес электронной почты или пароль')
+        return render_template('auth/auth.html', action='Авторизация', form=form)
+
+
+class LogoutView(MethodView):
+    decorators = [login_required]
+
+    def get(self):
+        logout_user()
+        flash('Вы успешно вышли из аккаунта', 'success')
+        return redirect(url_for('.login'))
