cc4ddf804bd7d205a3e9817d4b5180bb74b2f0b5
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2023-02-09 19:19:43 +0300

    = profile displaying for sqlalchemy

--- profile/views.py
+++ profile/views.py
@@ -2,7 +2,7 @@ from flask import redirect, url_for, render_template, flash, request
 from flask.views import MethodView
 from flask_login import login_required, logout_user, current_user
 
-from database import get_db
+from auth.models import User
 from profile import profile
 from profile.forms import ChangeDataForm
 
@@ -18,19 +18,18 @@ class PersonalView(MethodView):
 
     def get(self):
         form = ChangeDataForm()
-        form.email.data = current_user.user['email']
-        form.name.data = current_user.user['name']
+        form.email.data = current_user.email
+        form.name.data = current_user.name
         return render_template('profile/personal.html', page='personal', form=form)
 
     def post(self):
         form = ChangeDataForm()
         if form.validate_on_submit():
-            db = get_db()
-            name = form.name.data
-            user_id = current_user.user['id']
-            db.update('UPDATE users SET name = %s WHERE id = %s', (name, user_id))
-            current_user.user['name'] = name
-            flash('Информация успешно изменена', 'success')
+            user = User.query.get(current_user.id)
+            if user.update(name=form.name.data):
+                flash('Информация успешно изменена', 'success')
+            else:
+                flash('Информацию изменить не удалось, попробуйте позже', 'error')
         return render_template('profile/personal.html', page='personal', form=form)
 
 
