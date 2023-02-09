a60fc999eabb33ef121c11730a4702f43dcb5048
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2023-02-09 14:24:06 +0300

    = moved password setting and checking to model

--- auth/models.py
+++ auth/models.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from auth.enums import Role
 from database import db
@@ -11,6 +12,16 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(255))
     role = db.Column(db.Enum(Role), default=Role.user)
 
+    def __init__(self, password, **kwargs):
+        super().__init__(**kwargs)
+        self.set_password(password)
+
     @property
     def is_admin(self):
         return self.user.role == Role.admin
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
